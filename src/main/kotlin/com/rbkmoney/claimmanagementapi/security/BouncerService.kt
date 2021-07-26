9bd5bd9d831e5ef04496287640ea2dc04f176bdf
ElenaKushchenko <kushchenko.elena@gmail.com>
2021-07-26 16:16:52 +0300

    [JD-206] add logs (#14)

--- src/main/kotlin/com/rbkmoney/claimmanagementapi/security/BouncerService.kt
+++ src/main/kotlin/com/rbkmoney/claimmanagementapi/security/BouncerService.kt
@@ -2,6 +2,7 @@ package com.rbkmoney.claimmanagementapi.security
 
 import com.rbkmoney.bouncer.decisions.ArbiterSrv
 import com.rbkmoney.claimmanagementapi.config.properties.BouncerProperties
+import mu.KotlinLogging
 import org.apache.thrift.TException
 import org.springframework.stereotype.Service
 
@@ -12,11 +13,17 @@ class BouncerService(
     private val bouncerProperties: BouncerProperties
 ) {
 
+    private val log = KotlinLogging.logger { }
+
     fun havePrivileges(bouncerContext: BouncerContextDto): Boolean {
+        log.debug { "Check access with bouncer context: $bouncerContext" }
         return try {
             val context = bouncerContextFactory.buildContext(bouncerContext)
+            log.debug { "Built thrift context: $context" }
             val judge = bouncerClient.judge(bouncerProperties.ruleSetId, context)
+            log.debug { "Have judge: $judge" }
             val resolution = judge.getResolution()
+            log.debug { "Resolution: $resolution" }
             resolution.isSetAllowed
         } catch (e: TException) {
             throw BouncerException("Error while call bouncer", e)
