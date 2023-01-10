b71345afb2a2fb08bd2f26b7fb9ded52d6f041c2
Kostya Struga <kennny007@yandex.ru>
2023-01-10 21:30:56 +0700

    Fix party id

--- src/main/kotlin/com/rbkmoney/claimmanagementapi/service/PartyManagementService.kt
+++ src/main/kotlin/com/rbkmoney/claimmanagementapi/service/PartyManagementService.kt
@@ -3,10 +3,7 @@ package com.rbkmoney.claimmanagementapi.service
 import com.rbkmoney.claimmanagementapi.exception.client.ForbiddenException
 import com.rbkmoney.claimmanagementapi.exception.server.DarkApi5xxException
 import com.rbkmoney.claimmanagementapi.security.KeycloakService
-import dev.vality.damsel.payment_processing.InternalUser
 import dev.vality.damsel.payment_processing.PartyManagementSrv
-import dev.vality.damsel.payment_processing.UserInfo
-import dev.vality.damsel.payment_processing.UserType
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 
@@ -22,7 +19,7 @@ class PartyManagementService(
         val partyId = keycloakService.partyId
         log.info {
             "Trying to get request on party-management service for party-status, " +
-                "xRequestId='$xRequestId', partyId='$partyId'"
+                    "xRequestId='$xRequestId', partyId='$partyId'"
         }
         val status = getPartyStatus(xRequestId, partyId)
         if (status.getBlocking().isSetBlocked) {
@@ -33,13 +30,13 @@ class PartyManagementService(
         }
         log.info {
             "Request has been got on party-management service, " +
-                "party-status=unblocked, xRequestId='$xRequestId', partyId='$partyId'"
+                    "party-status=unblocked, xRequestId='$xRequestId', partyId='$partyId'"
         }
     }
 
     private fun getPartyStatus(xRequestId: String?, partyId: String) =
         try {
-            partyManagementClient.getStatus(userInfo, partyId)
+            partyManagementClient.getStatus(partyId)
         } catch (ex: Exception) {
             throw DarkApi5xxException(
                 "Some Exception while requesting api='party-management', method='getStatus', xRequestId=$xRequestId",
@@ -47,7 +44,4 @@ class PartyManagementService(
             )
         }
 
-    companion object {
-        private val userInfo = UserInfo("dark-api", UserType.internal_user(InternalUser()))
-    }
 }
